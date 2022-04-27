6cf2d207e1944e3d2ba5716c36ed1fdd2b80a005
martinJomt <104496177+martinJomt@users.noreply.github.com>
2022-04-27 12:15:00 +0200

    Add files via upload

--- package_001.dart
+++ package_001.dart
@@ -5,3 +5,12 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+void TextZeigen () {
+  
+  print("aUFRUF tEXTZEIGEN");
+  
+  print("Pamina meint auch was - kann sich aber nicht entscheiden was ...");
+  
+  print("Pamina: 'Ach, So!'");
+// TODO: https://github.com/flutter/samples/blob/master/animations/lib/main.dart gutes Textbeispiel
+}
